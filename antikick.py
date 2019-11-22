dc0c5cbd0ccb546f2f421d47d90d0e3526e1d6c9
Roqvie <vmandrch@gmail.com>
2019-11-22 14:56:04 +0300

    Главный файл v1.2

--- antikick.py
+++ antikick.py
@@ -3,26 +3,26 @@ import json
 from threading import Thread
 import sys
 
-print('reading "config.txt"')
-i = 0
-couple  = [{}, {}]
-file = open('config.txt', 'r')
-try:
-    for line in file:
-        acc = line.split()
-        print(acc)
-        couple[i]['id'] = acc[0]
-        couple[i]['token'] = acc[1]
-        i+=1
-    couple[0]['token'], couple[1]['token'] = couple[1]['token'], couple[0]['token']
+def get_id(token):
+    response = requests.get('https://api.vk.com/method/{method}?{params}&access_token={token}&v=5.95'.format(
+                            method = 'users.get',
+                            params = '',
+                            token = token)
+                            ).json()
+    if 'response' in response.keys():
+        id = str(response['response'][0]['id'])
+        first_name = response['response'][0]['first_name']
+        last_name = response['response'][0]['last_name']
+        print('\033[92m [+] Получен токен для %s (%s) - %s.. \033[0m' % (id, first_name, token[:10]))
+        return (id, first_name, last_name)
+    else:
+        print('\033[91m [-] Ответ от сервера не получен; Ошибка: \033[0m')
+        print(response['error']['error_msg'])
+        if len(token) != 85:
+            print('\033[91m [-] Длина токена должна быть = 85 \033[0m' % len(token))
+        raise ValueError
 
-except Exception as e:
-    print('Invalid id/token\nEdit "config.txt"')
-    print(e)
-    input('Press [Enter] to exit')
-    sys.exit()
-
-def antikick(token, tm_id):
+def antikick(token, def_id, name='...'):
     # get information about the server
     response = requests.get('https://api.vk.com/method/{method}?{params}&access_token={token}&v=5.95'.format(
                             method = 'messages.getLongPollServer',
@@ -33,41 +33,77 @@ def antikick(token, tm_id):
         ts = response['response']['ts']
         key = response['response']['key']
         server = response['response']['server']
+        print('\033[92m [+] Получен longpoll-сервер - {server} \033[0m'.format(server = server))
+        while True:
+            response = requests.get('https://{server}?act=a_check&key={key}&ts={ts}&wait=25&mode=2&version=3'.format(
+                                    server = server,
+                                    key = key,
+                                    ts = ts)
+                                    ).json()
+            ts = response['ts']
+            for event in response['updates']:
+                if event[0] == 52 and event[1] == 8:
+                    id = str(event[3])
+                    chat_id = event[2] - 2000000000
+                    print('\033[92m [+] %s (%s) кикнут из беседы %s \033[0m' % (name, id, chat_id))
+                    if id in def_ids:
+                        response = requests.get('https://api.vk.com/method/{method}?{params}&access_token={token}&v=5.95'.format(
+                                                method = 'messages.addChatUser',
+                                                params = 'chat_id={}&user_id={}'.format(chat_id, id),
+                                                token = token)
+                                                ).json()
+                        if 'response' in response.keys():
+                            if response['response'] == 1:
+                                print('\033[92m [+] %s возвращен в беседу \033[0m' % id)
+                        else:
+                            raise KeyError
+
     except KeyError as e:
-        print('Ошибка в ответе сервера - не хватает данных (ключа)')
+        print('\033[91m [-] Ошибка в ответе сервера - не хватает данных (ключа) \033[0m')
         print(e)
         print('\n', response)
     except Exception as e:
-        print('Some error ocurred:')
+        print('\033[91m [-] Неопознанная ошибка \033[0m')
         print(e)
         print('\n', response)
-    print('\nreceived longpool server:\n{server}'.format(server = server))
-    while True:
-        # connecting to longpool server
-        response = requests.get('https://{server}?act=a_check&key={key}&ts={ts}&wait=25&mode=2&version=3'.format(
-                                server = server,
-                                key = key,
-                                ts = ts)
-                                ).json()
-        # recieve first timestamp
-        ts = response['ts']
-        # receive events
-        for event in response['updates']:
-            if event[0] == 52 and event[1] == 8:
-                id = str(event[3])
-                chat_id = event[2] - 2000000000
-                print('User id%s has ben kicked from chat %s' % (id, chat_id))
-                if id == tm_id:
-                    response = requests.get('https://api.vk.com/method/{method}?{params}&access_token={token}&v=5.95'.format(
-                                            method = 'messages.addChatUser',
-                                            params = 'chat_id={}&user_id={}'.format(chat_id, tm_id),
-                                            token = token)
-                                            ).json()
-                    #print(response)
-                    if response['response'] == 1:
-                        print('def %s' % tm_id)
 
-defense_1 = Thread(target = antikick, args = (couple[0]['token'], couple[0]['id']))
-defense_2 = Thread(target = antikick, args = (couple[1]['token'], couple[1]['id']))
-defense_1.start()
-defense_2.start()
+
+print('\033[92m [+] Запись данных в  "config.txt" \033[0m')
+try:
+    file = open('config.txt', 'r')
+    # Файл уже существует
+    print('\033[92m [+] Найден файл конфига \033[0m')
+    ffstr = file.readlines()
+
+    if ffstr == [] or len(ffstr[0]) < 96 or len(ffstr) < 2:
+        print(ffstr)
+        print('\033[91m [-] Файл пустой, поврежденный иди в нем не хватает аккаунтов (минимум 2) \033[0m')
+    else:
+        tokens = []
+        ids = []
+        for line in ffstr:
+            id, token = line.split()
+            ids.append(id)
+            tokens.append(token)
+
+        for acc in tokens:
+            defense = Thread(target = antikick, args = (acc, ids))
+            defense.start()
+            
+# Файл неполный или поврежденный
+except FileNotFoundError as e:
+    print('\033[91m [-] Файл не найден или поврежден \033[0m')
+    file = open('config.txt', 'w')
+    cfg = {}
+    def_ids = []
+    tokens = []
+    for i in range(int(input('Введите кол-во аккаунтов - '))):
+        cfg[i] = {}
+        cfg[i]['token']  = input('Введите access_token %s-го аккаунта (85 символов) - ' % i)
+        cfg[i]['id'], cfg[i]['name'], _  = get_id(cfg[i]['token'])
+        def_ids.append(cfg[i]['id'])
+        tokens.append(cfg[i]['token'])
+        file.write('%s %s\n' % (cfg[i]['id'], cfg[i]['token']))
+    for acc in tokens:
+        defense = Thread(target = antikick, args = (acc, ids))
+        defense.start()
